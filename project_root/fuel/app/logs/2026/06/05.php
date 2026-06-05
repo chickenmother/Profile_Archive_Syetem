db52e6e8a201be6061226c86500241a098322ab7
chickenmother <as04117sky@gmail.com>
2026-06-05 19:14:09 +0900

    feat: track missing fuelphp framework core and public assets

---
+++ project_root/fuel/app/logs/2026/06/05.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2026-06-05 18:48:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
