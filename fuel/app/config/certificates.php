5f356f702e8b97785dcb51289e68358ee11cd78b
chickenmother <as04117sky@gmail.com>
2026-06-26 14:39:05 +0900

    feat: add dashboard route and new configuration files for certificates and skills

---
+++ fuel/app/config/certificates.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    1 => 'JLPT N1',
+    2=> 'JLPT N2',
+    3=> 'AWS Certified Solutions Architect',
+    4=> '基本情報技術者',
+    5=> '応用情報技術者',
+    6=> 'TOEIC 700+',
+);
---
+++ fuel/app/config/skills.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    1 => 'PHP',
+    2 => 'JavaScript',
+    3 => 'Python',
+    4 => 'Java',
+    5 => 'C#',
+    6 => 'C++',
+    7 => 'Ruby',
+    8 => 'Go',
+    9 => 'Swift',
+    10 => 'Kotlin',
+    11 => 'TypeScript',
+);
